6ca2f20301558cba28385bc7d26e438e031a5743
Hinokamika <manh.ndm2506@gmail.com>
2025-05-08 11:46:26 +0700

    commit

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from new-folder!")
+from email import message
+from dotenv import load_dotenv
+from typing import Annotated, Literal
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+from typing_extensions import TypedDict
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+llm = init_chat_model(
+    "gpt-3.5-turbo",
+)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph[State]
+
+def chatbot(state: State):
+    return {"messages" : [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter a message: ")
+state = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
+
+print(state["messages"][-1].content)
